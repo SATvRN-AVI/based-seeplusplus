bcf00d5c45f12b5c08be079d16f2677dc4f1c0ed
Walter Hudson <hudson.walterf@gmail.com>
2021-10-15 10:55:13 -0400

    Took notes on Ints, Values, Vars, Documentation

---
+++ NetAcad CPA/int-values-vars-comments.cpp
@@ -0,0 +1,66 @@
+/* ***********************************************************
+    Integers, Values, Variables, Documentation Notes
+    
+    Author: Walter Hudson, 2021
+
+    email: hudson.walterf@gmail.com
+
+    Changes:
+    
+    File created and saved 10-14-2021
+*********************************************************** */
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int counter;
+    int variable_1, account_balace, invoices;
+
+    counter = 1;
+
+    int variable = 1;
+
+    int twosquared = 2 * 2;
+
+    result = 100 + 200;
+
+    x = x + 1;
+
+
+
+}
+
+/* 
+Above is some practice with variables and integers and the assignment
+of values. On lines 6 and 7, we see a single integer declared with the
+name, "counter", and 3 integers declared in a single line declaration.
+
+Below that, we see that counter has been assigned the value of 1.
+
+We see on line 11 a new integer named "variable" assigned to the 1
+on the same line, rather than using two.
+
+On line 13 we declare an integer "twosquared" and assign it the value
+of 2 multiplied by 2. 
+
+On line 15, we see the variable "result" and set it equal to the sum
+of 100 plus 200. 
+
+On line 17 we see a variable "x" assigned the value of itself, plus
+1. This is useful for creating a counting sequence. 
+
+*/
+
+/* 
+Special note on Keywords! Keywords are words within the C++ syntax that
+perform a tasks or have values already assigned to them within the code.
+for example, you can't declare an int named "int", you could however,
+declare an integer named "Int" as C++ is case sensitive. However, this
+is considered incorrect, so it would be better for us to instead write
+"int_value" to differentiate. 
+*/
